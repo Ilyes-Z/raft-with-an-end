1b13f8e167777f34c8610c9dbb73ccfc1f340648
Emeric KPEGBA <emeric.kpegba@epitech.eu>
2021-01-31 15:10:47 +0100

    [FIX] mvt

--- mvt-e.gd
+++ mvt-e.gd
@@ -6,13 +6,13 @@ var velocity = Vector2()
 
 func get_input():
     velocity = Vector2()
-    if Input.is_action_pressed('right'):
+    if Input.is_action_pressed('ui-right'):
         velocity.x += 400
-    if Input.is_action_pressed('left'):
+    if Input.is_action_pressed('ui-left'):
         velocity.x -= 400
-    if Input.is_action_pressed('down'):
+    if Input.is_action_pressed('ui-down'):
         velocity.y += 400
-    if Input.is_action_pressed('up'):
+    if Input.is_action_pressed('ui-up'):
         velocity.y -= 400
     velocity = velocity.normalized() * speed
 
