c1d115b4b1bb40607a1b0c1b8787a65590a61be2
ilyes <ilyes.huus@gmail.com>
2021-01-31 17:12:32 +0100

    animated

--- GGJ/player.gd
+++ GGJ/player.gd
@@ -5,17 +5,21 @@ var motion = Vector2()
 func _physics_process(delta):
 	if Input.is_action_pressed("ui_right"):
 		motion.x = 4
+		get_node("AnimatedSprite").play()
 		get_node("AnimatedSprite").set_flip_h( false )
 	elif Input.is_action_pressed("ui_left"):
 		motion.x = -4
+		get_node("AnimatedSprite").play()
 		get_node("AnimatedSprite").set_flip_h( true )
 	elif Input.is_action_pressed("ui_up"):
 		motion.y = -4
+		get_node("AnimatedSprite").play()
 	elif Input.is_action_pressed("ui_down"):
 		motion.y = 4
+		get_node("AnimatedSprite").play()
 	else:
 		motion.x = 0
 		motion.y = 0
-		get_node("AnimatedSprite")
+		get_node("AnimatedSprite").stop()
 		
 	move_and_collide(motion)
