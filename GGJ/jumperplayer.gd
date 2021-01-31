f5f0d115f8af3b40032ce5d725a330b26a41daf2
JB <jean-baptiste.msika@epitech.eu>
2021-01-31 16:41:15 +0100

    runner

---
+++ GGJ/jumperplayer.gd
@@ -0,0 +1,20 @@
+extends KinematicBody2D
+
+var motion = Vector2()
+
+func _physics_process(delta):
+	motion.x = 0
+	if Input.is_action_pressed("movement_left"):
+		motion.x -= 600
+		get_node("AnimatedSprite").set_flip_h( true )
+	if Input.is_action_pressed("movement_right"):
+		motion.x += 600
+		get_node("AnimatedSprite").set_flip_h( false )
+	if Input.is_action_just_pressed("jump") && is_on_floor():
+		motion.y -= 1200
+	if motion.x != 0:
+		$AnimatedSprite.play()
+	else:
+		$AnimatedSprite.stop()
+	motion.y += 2400 * delta
+	motion = move_and_slide(motion, Vector2(0, -2))
